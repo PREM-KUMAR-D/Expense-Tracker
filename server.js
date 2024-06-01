337259022f284d0c6c090ce1d6b822ad1c6b32c2
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-06-01 08:32:29 +0530

    Completed adding different routes

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    const url =req.url;
+    res.setHeader('Content-Type','text/html');
+    if(url === '/' || url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>Server</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('<html>');
+        return res.end();
+    }else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>Server</title></head>');
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('<html>');
+        return res.end();
+    }else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>Server</title></head>');
+        res.write('<body><h1>Welcome to my nodeJS Project </h1></body>');
+        res.write('<html>');
+        return res.end();
+    }
 })
 
 server.listen(4000);
