74d82b4e17ecea4fb5bd6d73aaff2397af1c600f
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-06-09 11:38:40 +0530

    Made changes by implementing Backend myself edit functionality remaining

---
+++ Backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const expenseRoute = require('./routes/expense');
+const sequelize = require('./util/database');
+const Expense = require('./model/expense');
+
+
+
+
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/expense',expenseRoute);
+
+sequelize.sync({force:true});
+
+app.listen(4000);
---
+++ Backend/routes/expense.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expenseController = require('../controller/expense');
+
+
+
+const router = express.Router();
+
+
+
+router.get('/get-expenses',expenseController.getExpenses);
+
+router.post('/add-expense',expenseController.addExpense);
+
+router.get('/delete/:id',expenseController.deleteExpense)
+
+
+module.exports = router;
